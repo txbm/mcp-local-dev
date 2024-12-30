e4bdde763be4ddcf1ad68dbc0e352a54c5b8963f
Peter M. Elias <petermelias@gmail.com>
2024-12-29 23:54:23 -0800

    Remove unused sandbox and binary management complexity

--- src/mcp_runtime_server/binaries/__init__.py
+++ src/mcp_runtime_server/binaries/__init__.py
@@ -1,12 +1,1 @@
-"""Binary management and caching."""
-from mcp_runtime_server.binaries.fetcher import ensure_binary
-from mcp_runtime_server.binaries.cache import cleanup_cache
-from mcp_runtime_server.binaries.platforms import get_platform_info
-from mcp_runtime_server.binaries.constants import RUNTIME_BINARIES
-
-__all__ = [
-    "ensure_binary",
-    "cleanup_cache", 
-    "get_platform_info",
-    "RUNTIME_BINARIES"
-]
+# This module has been removed in favor of simpler implementation
