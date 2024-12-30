d5b2436bc783200f9e2a5edb25aeb42cf8b1de05
Peter M. Elias <petermelias@gmail.com>
2024-12-30 10:43:45 -0800

    Update runtime.py to add run_command and auto_run_tests imports

--- src/mcp_runtime_server/runtime.py
+++ src/mcp_runtime_server/runtime.py
@@ -1,2 +1,4 @@
 """Runtime system interface."""
 from mcp_runtime_server.environments import create_environment, cleanup_environment, ENVIRONMENTS
+from mcp_runtime_server.commands import run_command
+from mcp_runtime_server.execution import auto_run_tests
