5202eb3034c6564073cf55ed8f6db792d5895d63
Peter M. Elias <petermelias@gmail.com>
2024-12-30 00:07:52 -0800

    fix: restore sandbox exports

--- src/mcp_runtime_server/sandbox/__init__.py
+++ src/mcp_runtime_server/sandbox/__init__.py
@@ -1,1 +1,11 @@
-# This module has been removed in favor of simpler implementation
+"""Sandbox isolation functionality."""
+from mcp_runtime_server.sandbox.environment import create_sandbox, cleanup_sandbox, Sandbox
+from mcp_runtime_server.sandbox.security import apply_restrictions, remove_restrictions
+
+__all__ = [
+    "create_sandbox",
+    "cleanup_sandbox",
+    "Sandbox",
+    "apply_restrictions",
+    "remove_restrictions"
+]
