41c099c2aa9bf4459a21b28e47f6cb223b6c8ee9
Peter M. Elias <petermelias@gmail.com>
2024-12-30 10:32:47 -0800

    Remove auto_run_tests from __init__ imports

--- src/mcp_runtime_server/testing/__init__.py
+++ src/mcp_runtime_server/testing/__init__.py
@@ -1,2 +1,2 @@
+"""Testing package initialization."""
 from mcp_runtime_server.testing.frameworks import detect_frameworks, get_framework_command, parse_test_results
-from mcp_runtime_server.testing.execution import auto_run_tests
