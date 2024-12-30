09596353146d618026ee14bb5488ae3f3bf55c11
Peter M. Elias <petermelias@gmail.com>
2024-12-29 23:03:04 -0800

    fix: Update exports with normalized naming

--- src/mcp_runtime_server/__init__.py
+++ src/mcp_runtime_server/__init__.py
@@ -3,28 +3,57 @@
 from mcp_runtime_server.types import (
     RuntimeManager,
     RuntimeConfig,
+    Environment,
     TestConfig,
     TestResult,
-    TestRunResult,
     CaptureConfig,
     CaptureMode,
     ResourceLimits,
+    CapturedOutput
 )
 from mcp_runtime_server.sandbox import create_sandbox, cleanup_sandbox
 from mcp_runtime_server.binaries import ensure_binary
+from mcp_runtime_server.runtime import create_environment, cleanup_environment, run_command
+from mcp_runtime_server.errors import (
+    RuntimeServerError, 
+    EnvironmentError,
+    ResourceLimitError,
+    BinaryNotFoundError,
+    SandboxError
+)
 
 __version__ = "0.1.0"
 
 __all__ = [
+    # Runtime types
     "RuntimeManager",
     "RuntimeConfig",
+    "Environment",
+    
+    # Testing types
     "TestConfig",
     "TestResult",
-    "TestRunResult",
-    "CaptureConfig",
+    "CapturedOutput",
+    
+    # Configuration types
+    "CaptureConfig", 
     "CaptureMode",
     "ResourceLimits",
+    
+    # Runtime functions
+    "create_environment",
+    "cleanup_environment", 
+    "run_command",
+    
+    # Sandbox functions
     "create_sandbox",
     "cleanup_sandbox",
     "ensure_binary",
+    
+    # Error types
+    "RuntimeServerError",
+    "EnvironmentError",
+    "ResourceLimitError", 
+    "BinaryNotFoundError",
+    "SandboxError"
 ]
